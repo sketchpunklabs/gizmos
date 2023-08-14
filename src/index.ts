dcd8352d9261fda50bf2845f82f4e30a0bb5fee0
Pedro Sousa <sketchpunk@gmail.com>
2023-08-14 13:45:28 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Gizmos           from './Gizmos';
 import TranslateGizmo   from './gizmos/TranslateGizmo';
-import TwistGizmo      from './gizmos/TwistGizmo';
+import TwistGizmo       from './gizmos/TwistGizmo';
+import PointsGizmo      from './gizmos/PointsGizmo';
 
 export { 
     Gizmos,
     TranslateGizmo,
     TwistGizmo,
+    PointsGizmo,
 };
